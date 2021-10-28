7f755c8835511b41d567cc0842ce5d927688ab62
Arnab Biswas <khanp4397@gmail.com>
2021-10-28 17:32:56 +0530

    Added Transaction Route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var shareRouter = require('./routes/share');
 var globalRouter = require('./routes/global');
+var txdRouter = require('./routes/transaction.js');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/share', shareRouter);
 app.use('/global', globalRouter);
+app.use('/txd', txdRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
