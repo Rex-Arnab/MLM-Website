89c1d2bbf44b4d13c23f51e3fa9a0b60964ceac5
Arnab Biswas <khanp4397@gmail.com>
2021-10-28 17:30:10 +0530

    added global branch

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var shareRouter = require('./routes/share');
+var globalRouter = require('./routes/global');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/share', shareRouter);
+app.use('/global', globalRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
