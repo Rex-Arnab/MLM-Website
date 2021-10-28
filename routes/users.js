eb8ae97a3e352cfc818fb2b429a49761cffde161
Arnab Biswas <khanp4397@gmail.com>
2021-10-28 17:02:30 +0530

    Adding Base Routes : User

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,41 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+// /user/info
+// /user/level
+// /user/referel/count
+// /user/referel/code
+// /user/balance -> show bal
+// /user/balance/transfer -> main to activation
+// /user/balance/widthdrawl -> to bank
+
+// get user referel code
+router.get('/referel/code', function(req, res, next) {
+  res.send('respond with a resource');
+});
+// get user referel count
+router.get('/referel/count', function(req, res, next) {
+  res.send('respond with a resource');
+});
+// get user info
+router.get('/info', function(req, res, next) {
+  res.send('respond with a resource');
+});
+// get user level
+router.get('/level', function(req, res, next) {
+  res.send('respond with a resource');
+});
+// get user balance
+router.get('/balance', function(req, res, next) {
+  res.send('respond with a resource');
+});
+// get user balance transfer
+router.get('/balance/transfer', function(req, res, next) {
+  res.send('respond with a resource');
+});
+// get user balance withdrawl
+router.get('/balance/withdrawl', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
 module.exports = router;
